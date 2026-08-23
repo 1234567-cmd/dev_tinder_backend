a7e37a2205b599cec5b347ec1abe1fae6c05fc7d
waseem ahmad <waseemlone246@gmail.com>
2026-08-23 16:27:40 +0500

    slight change

--- src/app.js
+++ src/app.js
@@ -4,6 +4,16 @@ app.listen(3000,()=>{
   console.log("Server is set on port 3000");
 })
 
-app.use("/test",(req, res)=>{
-    res.send("Server is Responding Positive ")
-})
+// app.use("/test",(req, res)=>{
+//     res.send("Server is Responding Positive ")
+// })
+app.get("/user",(req,res,next)=>{
+  console.log(req.query)
+  res.send({
+    name:"Waseem Ahmad"
+  })
+  next()
+},((req,res)=>{
+  console.log("working")
+  res.send("Second")
+}))
