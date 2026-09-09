32a3057307859a8052d2026665ec0d198903df04
waseem ahmad <waseemlone246@gmail.com>
2026-09-09 19:48:30 +0500

    fix: correct ConnectionRequest import and improve user requests query

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
-const { ConnectionRequest } = require("../models/connectionRequest");
+const ConnectionRequest = require("../models/connectionRequest");
 
 
 userRouter.get('/user/requests/received', userAuth, async (req, res) => {
@@ -9,8 +9,30 @@ userRouter.get('/user/requests/received', userAuth, async (req, res) => {
         const user = req.user;
         const userRequests = await ConnectionRequest.find({
             toUserId: user._id,
-        });
+            status: "interested"
+        }).populate('fromUserId', ['firstName', 'lastName']); // Populate the fromUserId field with user details
         res.status(200).json({ requests: userRequests });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try {
+        const user = req.user;
+        const connections = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: user._id, status: "accepted" },
+                { toUserId: user._id, status: "accepted" }
+            ]
+        }).populate('fromUserId toUserId', ['firstName', 'lastName']);
+        res.status(200).json({ connections });
+        const data = connections.map(connection => {
+            connection.fromUserId
+        }
+        )
+        res.status(200).json({ connections: data });
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
