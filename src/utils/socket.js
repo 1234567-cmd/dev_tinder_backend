50ad926045362d3d16733e07cf2d064fd5ebb62d
waseem ahmad <waseemlone246@gmail.com>
2026-09-22 08:30:58 +0500

    fix: format code and improve socket connection handling

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,17 +1,25 @@
 
-const socket=require("socket.io")
+const socket = require("socket.io")
 
-const initializeSocket=(server)=>{
-    const io=socket(server,{
-        cors:{
-            origin:process.env.CORS_ORIGIN,
-            methods:["GET","POST"],
-            credentials:true
+const initializeSocket = (server) => {
+    const io = socket(server, {
+        cors: {
+            origin: process.env.CORS_ORIGIN,
+            methods: ["GET", "POST"],
+            credentials: true
         }
     })
-   io.on("connection",(socket)=>{
-        console.log("Socket connected",socket.id)
-   })
+    io.on("connection", (socket) => {
+        socket.on("joinChat", () => {
+            console.log("User joined the chat")
+        })
+        socket.on("sendMessage", () => {
+            console.log("Message sent")
+        })
+        socket.on("disconnect", () => {
+            console.log("User disconnected")
+        })
+    })
 }
 
-module.exports=initializeSocket
+module.exports = initializeSocket
